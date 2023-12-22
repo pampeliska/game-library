ddd1b499987a34aae43435350613517eb4352e4b
Jana Šnirychová <jana.snirychova@artin.cz>
2023-12-22 15:57:16 +0100

    Images optimized

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
@@ -12,7 +13,7 @@ const GameCard = (props: Props) => {
 
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+    const target = "media/";
+    // url https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+    // url smaller size https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+    const index =  url.indexOf(target) + target.length; //put changes behind target
+    return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+}
+
+export default getCroppedImageUrl;
