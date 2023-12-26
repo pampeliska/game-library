7fd99d73ed1095459473f05628cb6029d122f407
Jana Šnirychová <jana.snirychova@artin.cz>
2023-12-26 19:22:54 +0100

    Removing duplicate styles refactoring

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const GameCardContainer = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
