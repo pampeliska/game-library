90c95babbb2d68c07b8a9141cd446696f6d7ec76
Jana Šnirychová <jana.snirychova@artin.cz>
2023-12-31 19:12:35 +0100

    Extracting query object refactoring

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,17 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
+import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
-import { Genre } from "../hooks/useGenres";
-import { Platform } from "../hooks/usePlatforms";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
 const GameGrid = (props: Props) => {
-  const { selectedGenre, selectedPlatform } = props;
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+  const { gameQuery } = props;
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [...Array(15).keys()];
 
   return (
