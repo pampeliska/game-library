24303e4c2b82a2a38025a1eca38dd62fd34ebe2d
Jana Šnirychová <jana.snirychova@artin.cz>
2024-01-01 20:41:12 +0100

    Sort games according to various fields

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return "";
+    
     const target = "media/";
     // url https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
     // url smaller size https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
