1c9117b72b07e73bcdaf95b71a80943388cc9b6f
Jana Šnirychová <jana.snirychova@artin.cz>
2024-01-02 12:00:42 +0100

    Searching functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,16 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = (props: Props) => {
+  const { onSearch } = props;
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
